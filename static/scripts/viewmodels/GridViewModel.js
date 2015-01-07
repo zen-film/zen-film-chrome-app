f6ef1101005e7af233cb13f0d70967a697d90967
Maxim Syrykh (@slggr) <slogger1994@gmail.com>
2015-01-07 21:23:31 +0500

    Add editor block

--- static/scripts/viewmodels/GridViewModel.js
+++ static/scripts/viewmodels/GridViewModel.js
@@ -6,12 +6,35 @@ define(
         function GridViewModel(){
             var self = this;
             self.photos = ko.observableArray([]);
+            self.selectPhotos = ko.observableArray([]);
+
             self.loadPhoto = function() {
                 $.getJSON("/photos").done(
                     function(data) {
                         self.photos(data);
                     }
                 );
+            };
+
+            self.selectToggle = function() {
+                this.domElem = $("img[src=" + "\'/photo/" + this.SourceFile + "\']");
+                this.domElem.parent().toggleClass("photo_selected");
+                self.selectPhotos.append(this);
+            };
+
+            self.selectAllToggle = function() {
+                // TODO: FIX THEM
+                var allImage = $("img"),
+                    selectedStatus = allImage.parent().forEach(function(index) {
+                        return allImage[index].classList.contains("photo_selected");
+                    });
+                console.log(selectedStatus);
+
+                // allImage.parent().toggleClass("photo_selected", !allSelected);
+            };
+
+            self.runEditor = function() {
+
             };
         }
         return GridViewModel;
