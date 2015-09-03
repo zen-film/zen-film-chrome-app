4cb07e3ebf3a410fd5ce6f785852913b20f21af3
Maxim Syrykh <slogger1994@gmail.com>
2015-09-04 03:44:48 +0500

    base layout

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,63 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
-  }
-}, false);
+
+(function(document) {
+    var highlights = document.querySelector('.selection-highlights');
+    var content = document.querySelector('.selection-content');
+    var fileLoader = document.querySelector('#file-loader');
+
+    var toggleCheckboxHandler = function() {
+        var label = document.querySelector('label[for="' + this.getAttribute('id') + '"]');
+        label.setAttribute('data-checked', this.getAttribute('checked'));
+    };
+
+    [].forEach.call(document.querySelectorAll('.selection-checkbox'), function(node) {
+        node.addEventListener('change', toggleCheckboxHandler, false);
+    });
+
+    var fileSelectHandle = function(event) {
+        var photos = event.target.files;
+        for (var i = 0, f; f = photos[i]; i++) {
+
+            // Only process image files.
+            if (!f.type.match('image.*')) {
+                continue;
+            }
+
+            var reader = new FileReader();
+
+            // Closure to capture the file information.
+            reader.onload = (function(theFile) {
+                return function(e) {
+                    var img = e.target.result;
+
+                    var id = theFile.name;
+
+                    var checkbox = document.createElement('input');
+                    checkbox.setAttribute('type', 'checkbox');
+                    checkbox.classList.add('selection-checkbox');
+                    checkbox.id = id;
+                    highlights.insertBefore(checkbox, fileLoader);
+
+                    var highlight = document.createElement('div');
+                    highlight.classList.add('selection-highlight');
+                    highlights.insertBefore(highlight, fileLoader);
+
+                    var label = document.createElement('label');
+                    label.setAttribute('for', id);
+                    label.classList.add('selection-item');
+                    content.insertBefore(label, fileLoader.labels[0]);
+
+                    var imgContainer = document.createElement('span');
+                    imgContainer.classList.add('selection-item-container');
+                    imgContainer.style.backgroundImage = 'url(' + img + ')';
+                    label.appendChild(imgContainer);
+                };
+            })(f);
+
+            // Read in the image file as a data URL.
+            reader.readAsDataURL(f);
+        }
+    };
+
+    fileLoader.addEventListener('change', fileSelectHandle, false);
+})(document);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,16 +2,18 @@
 
 // Listens for the app launching then creates the window
 chrome.app.runtime.onLaunched.addListener(function() {
-  var width = 500;
-  var height = 300;
+    var width = 900;
+    var height = 600;
 
-  chrome.app.window.create('index.html', {
-    id: 'main',
-    bounds: {
-      width: width,
-      height: height,
-      left: Math.round((screen.availWidth - width) / 2),
-      top: Math.round((screen.availHeight - height)/2)
-    }
-  });
+    chrome.app.window.create('index.html', {
+        id: 'main',
+        bounds: {
+            width: width,
+            height: height,
+        },
+        innerBounds: {
+            minWidth: width,
+            minHeight: height
+        }
+    });
 });
